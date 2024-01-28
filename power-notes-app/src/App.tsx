89d1f0d65c31b3d8dfe6d73e743dcc1e965d2b33
Will Hammond <wsh2117@columbia.edu>
2024-01-28 16:38:42 -0500

    Add left  menu with toggle open and close

--- power-notes-app/src/App.tsx
+++ power-notes-app/src/App.tsx
@@ -5,9 +5,14 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Container } from "@mui/material";
+import { PersistentLeftMenu } from "./navigation-components/PersistentLeftMenu";
 
 function App() {
-  return <Container>Body here!</Container>;
+  return (
+    <Container sx={{}}>
+      <PersistentLeftMenu />
+    </Container>
+  );
 }
 
 export default App;
